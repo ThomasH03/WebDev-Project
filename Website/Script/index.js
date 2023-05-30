a970b56ea998ab9a73f9726a99d514ff7b5abe2e
ThomasH03 <115984651+ThomasH03@users.noreply.github.com>
2023-05-30 11:51:30 +0100

    Minor Changes
    during the video i noticed a few things that needed to be fixed so changed it during these will be minor fixes like removing redundant code and fixing spelling mistakes

--- Website/Script/index.js
+++ Website/Script/index.js
@@ -1,19 +1,21 @@
-// Marketplace calculation of the total price
+// Function to calculate the total price
 function calculateTotalPrice() {
-    // Get all the buttons the Add to Cart buttons
+
+    // Get all the "Add to Cart" buttons
     const buttons = document.querySelectorAll('button');
-    
+  
     let totalPrice = 0;
-    
+  
     // Loop through each button
-    buttons.forEach(button => {
+    buttons.forEach((button) => {
       button.addEventListener('click', () => {
+        
         // Get the price from the <p> tag
         const price = parseFloat(button.previousElementSibling.textContent.split(':')[1].trim().replace('£', ''));
-        
+  
         // Add the price to the total
         totalPrice += price;
-        
+  
         // Display the total price
         alert(`Total Price: £${totalPrice.toFixed(2)}`);
       });
