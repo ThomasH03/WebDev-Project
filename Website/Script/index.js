0ab0fab3bd75bb9259c4a231e08e6c089091925b
ThomasH03 <115984651+ThomasH03@users.noreply.github.com>
2023-05-30 10:13:14 +0100

    Add To Cart
    in this one i added some JS and made the marketplace page be more interactive with them now being able to add the items to their basket

---
+++ Website/Script/index.js
@@ -0,0 +1,25 @@
+// Marketplace calculation of the total price
+function calculateTotalPrice() {
+    // Get all the buttons the Add to Cart buttons
+    const buttons = document.querySelectorAll('button');
+    
+    let totalPrice = 0;
+    
+    // Loop through each button
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        // Get the price from the <p> tag
+        const price = parseFloat(button.previousElementSibling.textContent.split(':')[1].trim().replace('£', ''));
+        
+        // Add the price to the total
+        totalPrice += price;
+        
+        // Display the total price
+        alert(`Total Price: £${totalPrice.toFixed(2)}`);
+      });
+    });
+  }
+  
+  // Call the function when the page has finished loading
+  window.addEventListener('load', calculateTotalPrice);
+  
